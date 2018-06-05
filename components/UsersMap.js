cdd9f95050f0378b3a17d368399cf360e1b0c14f
logan <logan.christopher@comcast.net>
2018-06-05 15:54:55 -0500

    Finished up comments on project

--- components/UsersMap.js
+++ components/UsersMap.js
@@ -16,6 +16,17 @@ const UsersMap = props => {
     userLocationMarker = <MapView.Marker coordinate={props.userLocation}/>
   }
 
+  // Although the newly set state of the other users has been generated and stored within an array, those coords wont be much help without a marker.
+  // The following displays markers for every coord logged within firebase (ie. other users).
+  // new constant is created to display all other users locations within a new mapview marker element.
+  const otherUsersMarkers = props.otherUsersLocations.map(otherUsersMarker => ( // props.otherUsersLocations is a global prop created w/n App class.
+    <MapView.Marker coordinate={otherUsersMarker} key={otherUsersMarker.id}/> // const is taking the array of data w/n prop: otherUsersLocations, and adding it to the map method which will display the MapView.Marker JSX element for every obj w/n the array
+  )); // functional component is called otherUsersMarker ~not~ otherUsersMarkers (this is the const name) and it is equal to the following JSX element
+      // The marker takes a coordinate argument and a key argument...
+      // the coordinate arg is technically equal to an individual index within the array: otherUsersLocations, bound to the prop: otherUsersLocations which is then assigned to the map methods parent: otherUsersMarker...same concept applies below
+      // coordinate arg is technically equal to otherUsersLocationsArray...function loops through and grabs both lat and long and just ignores id
+      // the id arg is technically equal to the id stored within otherUsersLocationsArray.id
+
   // The following return statement is what is returned by the default const for the page: UsersMap.
   // It contains a MapView element wrapped within a View JSX element.
   return (
@@ -32,8 +43,8 @@ const UsersMap = props => {
         region={props.userLocation}
         style={styles.Map}>
 
-        {/* Line below is a conditionally rendered variable (let: see above) */}
-        {userLocationMarker}
+        {userLocationMarker} {/* This is a conditionally rendered variable (let: see above) */}
+        {otherUsersMarkers} {/* const can then be outputted to the MapView */}
       </MapView>
     </View>
   )
